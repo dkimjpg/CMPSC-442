be0d726159a14aa325f8672dd575bfb3dc07740f
David <dkk5396@psu.edu>
2024-04-01 22:12:22 -0400

    Uploading homework 5 files.

---
+++ Homework 5/homework5_dkk5396.py
@@ -0,0 +1,36 @@
+############################################################
+# CMPSC442: Classification
+############################################################
+
+student_name = "David Kim"
+
+############################################################
+# Imports
+############################################################
+
+# Include your imports here, if any are used.
+
+############################################################
+# Section 1: Spam Filter
+############################################################
+
+def load_tokens(email_path):
+    pass
+
+def log_probs(email_paths, smoothing):
+    pass
+
+class SpamFilter(object):
+
+    def __init__(self, spam_dir, ham_dir, smoothing):
+        pass
+    
+    def is_spam(self, email_path):
+        pass
+
+    def most_indicative_spam(self, n):
+        pass
+
+    def most_indicative_ham(self, n):
+        pass
+
