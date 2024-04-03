2609109092bf90dc9768f448029211cfd02f0380
David <dkk5396@psu.edu>
2024-04-03 02:27:20 -0400

    After some trouble, fiddling, and Googling, I managed to solve the first part.
    Now as it turns out, it seems that the professor neglected to mention that it was necessary to use open() on the given email_path before using the email.message_from_file() method. I spent 30 minutes trying to figure this one out and only found out the answer after googling and finding out why the method wouldn't work. Since I kept getting an error on "str does not have .read()" or something like that, I had my suspicions, but I assumed that the method would have automatically opened it.
    
    Well, it doesn't. Anyway, once I figured that part out, it didn't take long for me to figure the rest out. Ech.

--- Homework 5/homework5_dkk5396.py
+++ Homework 5/homework5_dkk5396.py
@@ -9,13 +9,26 @@ student_name = "David Kim"
 ############################################################
 
 # Include your imports here, if any are used.
+import email
+import email.iterators
+import email.policy
+from email.policy import default
 
 ############################################################
 # Section 1: Spam Filter
 ############################################################
 
 def load_tokens(email_path):
-    pass
+    tokenList = []
+
+    openMessage = open(email_path, 'r') #without this, none of the other email methods work. Would've been good to know beforehand.
+    emailMessage = email.message_from_file(openMessage) 
+
+    for part in email.iterators.body_line_iterator(emailMessage): #traverses the message line by line, and each line is an str
+        line = part.split() #part is an str, so it can be split into a list
+        tokenList.extend(line)
+    return tokenList
+    #pass
 
 def log_probs(email_paths, smoothing):
     pass
@@ -34,3 +47,10 @@ class SpamFilter(object):
     def most_indicative_ham(self, n):
         pass
 
+ham_dir = "homework5_data/train/ham/"
+print(load_tokens(ham_dir+"ham1")[200:204])
+#print(load_tokens(ham_dir+"ham1"))
+print(load_tokens(ham_dir+"ham2")[110:114])
+spam_dir = "homework5_data/train/spam/"
+print(load_tokens(spam_dir+"spam1")[1:5])
+print(load_tokens(spam_dir+"spam2")[:4])
