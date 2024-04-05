92eb11416da525c1191b6bee83f3d852e20fe4bf
David <dkk5396@psu.edu>
2024-04-04 23:12:05 -0400

    Question 2 runs, but the output is wrong.
    Maybe there's something wrong with how I implemented the equation, but I'm just not sure what it could be... I also fixed a bug with opening the file by adding encoding="utf8" to the open() in load_tokens().

--- Homework 5/homework5_dkk5396.py
+++ Homework 5/homework5_dkk5396.py
@@ -13,6 +13,7 @@ import email
 import email.iterators
 import email.policy
 from email.policy import default
+import math
 
 ############################################################
 # Section 1: Spam Filter
@@ -20,8 +21,9 @@ from email.policy import default
 
 def load_tokens(email_path):
     tokenList = []
+    #policy = email.policy.EmailPolicy(utf8=True)
 
-    openMessage = open(email_path, 'r') #without this, none of the other email methods work. Would've been good to know beforehand.
+    openMessage = open(email_path, encoding="utf8") #without this, none of the other email methods work. Would've been good to know beforehand.
     emailMessage = email.message_from_file(openMessage) 
 
     for part in email.iterators.body_line_iterator(emailMessage): #traverses the message line by line, and each line is an str
@@ -33,20 +35,35 @@ def load_tokens(email_path):
 def log_probs(email_paths, smoothing):
     #need to build a dictionary and fill the values with calculations
     #floatSmoothing = float(smoothing) #converts string for smoothing into a float, if it is not already a float
-    #print(type(smoothing))
     probDict = {}
     fullTokenList = []
     for path in email_paths:
         emailTokenList = load_tokens(path)
         fullTokenList.extend(emailTokenList)
     
+    fullTokenSet = set(fullTokenList)
+    sigmaCount = len(fullTokenSet)       #this is the value of Sigma count(w')
+    fullCount = len(fullTokenList)       #this is the value of |V|
+    print(sigmaCount)
+    print(fullCount)
+
     #from here, I should iterate through emailTokenList and run the Laplace probabilities and put the results in a dictionary (if the key is not already in the dictionary, I think)
     # Ok, so after talking to some people, count(w) is just the number of occurances of a single word, which I already knew.
     # But Sigma count(w') is the count of all unique words, while |V| is just the length of the entire list (or in other words, the count of EVERY word, whether it's unique or not).
     # And a (which is really alpha) is just the smoothing var.
-    for token in fullTokenList:
-        print("")
-
+    #for token in fullTokenList:
+    for token in fullTokenSet: #if this doesn't work well, use fullTokenList
+        tokenCount = fullTokenList.count(token) #this is the value of count(w)
+        #print(token)
+        #print(smoothing)
+        calcProb = (tokenCount + smoothing) / (sigmaCount + (smoothing * (fullCount + 1))) #this is the equation for P(w)
+        #print(f'calcProb: {calcProb}')
+        calcProb = math.log(calcProb)
+        probDict.update({token: calcProb})
+    calcUNK = smoothing / (sigmaCount + (smoothing * (fullCount + 1)))                     #this is the equation for P(<UNK>)
+    calcUNK = math.log(calcUNK)
+    probDict.update({"<UNK>": calcUNK})
+    return probDict
     #pass
 
 class SpamFilter(object):
@@ -63,6 +80,14 @@ class SpamFilter(object):
     def most_indicative_ham(self, n):
         pass
 
+
+
+#####################################################
+# Test Cases
+#####################################################
+
+print("Question 1\n")
+
 ham_dir = "homework5_data/train/ham/"
 print(load_tokens(ham_dir+"ham1")[200:204])
 #print(load_tokens(ham_dir+"ham1"))
@@ -70,3 +95,16 @@ print(load_tokens(ham_dir+"ham2")[110:114])
 spam_dir = "homework5_data/train/spam/"
 print(load_tokens(spam_dir+"spam1")[1:5])
 print(load_tokens(spam_dir+"spam2")[:4])
+
+print("\nQuestion 2\n")
+
+paths = ["homework5_data/train/ham/ham%d" % i for i in range(1, 11)]
+p = log_probs(paths, 1e-5)
+print(p["the"])
+print(p["line"])
+
+print("")
+paths = ["homework5_data/train/spam/spam%d" % i for i in range(1, 11)]
+p = log_probs(paths, 1e-5)
+print(p["Credit"])
+print(p["<UNK>"])
