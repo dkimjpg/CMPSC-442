7ca0b8d7d0f3bbac8e88163a358e77d1492d1d62
David <dkk5396@psu.edu>
2024-04-15 18:26:34 -0400

    Committing the files for Homework 6.

---
+++ Homework 6/homework6_cmpsc442.py
@@ -0,0 +1,32 @@
+############################################################
+# CMPSC 442: Hidden Markov Models
+############################################################
+
+student_name = "Type your full name here."
+
+############################################################
+# Imports
+############################################################
+
+# Include your imports here, if any are used.
+
+
+
+############################################################
+# Section 1: Hidden Markov Models
+############################################################
+
+def load_corpus(path):
+    pass
+
+class Tagger(object):
+
+    def __init__(self, sentences):
+        pass
+
+    def most_probable_tags(self, tokens):
+        pass
+
+    def viterbi_tags(self, tokens):
+        pass
+
